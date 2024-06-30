125715a43e0a2794350df325cb396d43df426d9c
Renee bGagnon <150386461+reneemgagnon@users.noreply.github.com>
2024-06-29 17:27:45 -0700

    Update example_usage.py

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,3 +1,11 @@
+"""
+Striped LSTM Implementation
+Copyright (c) 2024 Renee M Gagnon
+This script is dual-licensed. See the LICENSE.md file for details.
+For non-commercial and research use: CC BY-NC 4.0
+For commercial use: Contact renee@freedomfamilyconsulting.ca
+"""
+
 import torch
 from src.zebra_lstm import StripedLSTM
 
